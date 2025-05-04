d888a5ebbba6653c85d55f81203475c38e2129e0
Jose Fernández Tamame <jftamames@gmail.com>
2025-05-04 08:39:39 +0000

    Describe aquí tus últimos cambios

--- verdiktia/__init__.py
+++ verdiktia/__init__.py
@@ -3,17 +3,14 @@
 """
 Verdiktia: paquete principal que expone las funciones clave.
 """
-
 from .data import get_countries
 from .logic import load_weights, score_country, rank_countries
 from .ui import render_inputs, render_weights, render_results
+from .inquiry import InquiryEngine
 
 __all__ = [
     "get_countries",
-    "load_weights",
-    "score_country",
-    "rank_countries",
-    "render_inputs",
-    "render_weights",
-    "render_results",
+    "load_weights", "score_country", "rank_countries",
+    "render_inputs", "render_weights", "render_results",
+    "InquiryEngine",
 ]
