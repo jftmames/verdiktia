3f5f46a17584ab1a148969c62b29843047e86b47
Jose Fernández Tamame <jftamames@gmail.com>
2025-05-04 09:28:01 +0000

    Describe aquí tus últimos cambios

--- verdiktia/__init__.py
+++ verdiktia/__init__.py
@@ -1,18 +1,13 @@
-from .data    import get_countries
-from .logic   import load_weights, score_country, rank_countries
-from .ui      import render_inputs, render_weights, render_results
-from .inquiry import InquiryEngine
+from .data       import get_countries
+from .logic      import load_weights, score_country, rank_countries
+from .ui         import render_inputs, render_weights, render_results, render_canvas, render_reasoning_graph, render_adaptations
+from .inquiry    import InquiryEngine
+from .adaptation import AdaptationEngine
 
 __all__ = [
     "get_countries",
     "load_weights", "score_country", "rank_countries",
     "render_inputs", "render_weights", "render_results",
-    "InquiryEngine",
+    "render_canvas", "render_reasoning_graph", "render_adaptations",
+    "InquiryEngine", "AdaptationEngine",
 ]
- from .inquiry import InquiryEngine
-+from .adaptation import AdaptationEngine
-
- __all__ = [
-     ...,
-+    "AdaptationEngine",
- ]
